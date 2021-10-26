3f199687f1cfdd00f6404b2d7ebdd04f0c813d08
hai anh <anhndhse151389@fpt.edu.vn>
2021-10-26 20:41:11 +0700

    update material

---
+++ material/majorData.sql
@@ -0,0 +1,17 @@
+
+
+USE MAS
+
+INSERT	INTO Major(Id, Name, Description)
+VALUES	('SS', N'Unknown',null),
+('SA', N'Unknown',null),
+('SE', N'SoftwareEngineering',null),
+('IA', N'InformationAssurance',null),
+('AI', N'ArtificalIntelligence',null),
+('HM', N'HotelManagement',null),
+('BA', N'BusinessAdministration',null),
+('GD', N'GraphicDesign',null),
+('MC', N'Multimediacommunication',null),
+('IB', N'InternationalBussiness',null),
+('JP', N'JapaneseLanguage',null),
+('EN', N'EnglishLanguage',null);
