2c75e8cad5adc3f90b8b9a9d00c0e70ccdd9a6c3
ledanhtrong <90688469+ledanhtrong@users.noreply.github.com>
2021-11-24 10:58:55 +0700

    Update data

--- materials/adminAccount.sql
+++ materials/adminAccount.sql
@@ -2,4 +2,4 @@
 USE MAS
 
 INSERT INTO Admin(Username, Password, Name, Phone, Email, Gender, Birthday, AvatarURL)
-VALUES ('admin', 123, N'Lê Hồng Phong', '0901481153', 'wifildt@gmail.com', 1, null, null);
+VALUES ('admin', '$2a$10$RswSEW6.X1RRAkjJkO1pd.Infesb/OFq7u9nZ0lvC8amdSjabC2/G', N'Lê Hồng Phong', '0901481153', 'wifildt@gmail.com', 1, null, null);
